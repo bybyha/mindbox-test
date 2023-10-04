cdb9497db26adc0814b0a6a5a152284c9f64ae93
bybyha <bybyha@tut.by>
2023-10-04 15:39:22 +0300

    Add files via upload

---
+++ Figures.cs
@@ -0,0 +1,41 @@
+﻿namespace Figures;
+
+abstract class Figure // абстрактный класс фигуры
+{
+    public abstract double Area(); // абстрактный метод для реализации в наследуемых классах
+}
+class Triangle : Figure // класс треугольника 
+{
+    public double side_a, side_b, side_c;
+    public Triangle(double side_a, double side_b, double side_c)
+    {
+        this.side_a = side_a;
+        this.side_b = side_b;
+        this.side_c = side_c;
+    }
+    public override double Area()
+    {
+        var s = GetSemiperimeter();
+        return Math.Sqrt(s * (s - side_a) * (s - side_b) * (s - side_c));
+    }
+    public bool IsRight() // метод проверки на прямоугольность
+    {
+        return (side_a * side_a + side_b * side_b == side_c * side_c) || (side_a * side_a + side_c * side_c == side_b * side_b) || (side_c * side_c + side_b * side_b == side_a * side_a); 
+    }
+    private double GetSemiperimeter()
+    {
+        return (side_a + side_b + side_c) / 2;
+    }
+}
+class Circle : Figure // класс круга
+{
+    public const double pi = 3.141592;
+    public double radius;
+    public Circle(double radius) { this.radius = radius; }
+    public override double Area()
+    {
+        return Math.Round(pi * (radius * radius),2);
+    }
+}
+
+
---
+++ FiguresTest.cs
@@ -0,0 +1,46 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Figures;
+
+namespace FiguresTest;
+
+[TestClass]
+public class FigureTest
+{
+    [TestMethod]
+    public void Triangle_Area_ValidCalc() // Тест на площадь треугольника
+    {
+        double side_a = 3;
+        double side_b = 4;
+        double side_c = 5;
+        Triangle triangle = new(side_a, side_b, side_c);
+
+        double area = triangle.Area();
+
+        Assert.AreEqual(area, 6, "Incorrect triangle area");
+    }
+
+        [TestMethod]
+    public void Triangle_IsRight_Valid() // Тест на прямоугольность
+    {
+
+        Triangle triangle = new(3, 4, 5);
+        Triangle triangle2 = new(3, 7, 2);
+
+
+        double area = triangle.Area();
+
+        Assert.IsTrue(triangle.IsRight()); // передаем прямоугольный
+        Assert.IsFalse(triangle2.IsRight()); // передаем не прямоугольный
+    }
+
+    [TestMethod]
+    public void Circle_Area_ValidCalc() // Тест на площадь круга
+    {
+        double radius = 7;
+        Circle circle = new(radius);
+
+        double area = circle.Area();
+
+        Assert.AreEqual(area, 153.94, "Incorrect circle area");
+    }
+} 
